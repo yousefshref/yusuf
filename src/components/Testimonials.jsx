4e141e5e90c3952fa9ac74adce40d4815b378ef8
yousefshref <yb2005at@gmail.com>
2024-10-03 11:52:57 +0300

    ds

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -13,19 +13,19 @@ const Testimonials = () => {
             "name": "محمد حسن",
             "jobTitle": "بوكر في شركة عقارات",
             "comment": "كنت محتاج موقع اعرض كل اللي عندي ويكون في filter عشان الclients يعرفو يجيبو كل اللي هم عايزينو منغير ما يسأل وانا ادور على اللي طلبو وياخد وقت كتير جدا",
-            "image": "/images/yusuf.jpeg",
+            "image": "https://scontent.fcai19-7.fna.fbcdn.net/v/t39.30808-6/441220319_1148718349806885_3080307027103083179_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=DqY2N7dDYLMQ7kNvgGM2C3j&_nc_ht=scontent.fcai19-7.fna&_nc_gid=ANg8ZXVu4bLEUIeyBKlkmly&oh=00_AYAX-VlKub3Hhki_wN20PULeaD6lSBQOHe9UMShf0nXc_A&oe=67042BFD",
         },
         {
             "name": "الكوتش عبد الرحيم",
             "jobTitle": "صاحب جيم",
             "comment": "دورت على اكتر من شركة واكتر من حد يقدر يركبلي سيستم دخول وخروج الموظفين وكارنيهات الclients بمجر مرورهم من الباب وفعلا لقيت حاجة سهله الاستخدام وحلوة جدا",
-            "image": "/images/yusuf.jpeg",
+            "image": "https://scontent.fcai19-7.fna.fbcdn.net/v/t39.30808-6/453773015_2716844338494604_3152240010412588052_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=1wlF6sLgX3YQ7kNvgFKuPDr&_nc_ht=scontent.fcai19-7.fna&_nc_gid=AAclgoN9_wJqwUkhoxsgP0f&oh=00_AYAjF3HlSrCWiXgo0eG3l-z09eILo9WKEKh_3XjyBlI3Hg&oe=670435FB",
         },
         {
             "name": "المهندس مصطفى",
             "jobTitle": "صاحب عمل حر تجارة الكترونية",
             "comment": "كنت شغال على wordpress بقالي فترة بس مكنش مريحني في حساب المرتجعات والارباح والمخازن بطريقة مريحة وكنت دايما بتلخبط.. بس الحمدلله يوسف عملي حاجة سهله جدا جدا وحلوة ساعدتني كتير بقالي اكتر حوالي سنتين شغال عليها بارتياح",
-            "image": "/images/yusuf.jpeg",
+            "image": "https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg",
         },
     ]
 
